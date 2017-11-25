c290bafeaf38240dbb7e46de34ef32ede27aa9ba
Troy Scott <consult@troyscott.ca>
2017-11-25 23:05:31 +0900

    added optional styling for mesages

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -11,7 +11,10 @@ function LunchCheckController($scope){
     $scope.message = "";
     $scope.lunchList = "";
     $scope.checkIfTooMuch = checkIfTooMuch;
-
+    // No style intially
+    $scope.messageStyle = {};
+    console.log($scope.messageStyle);
+    
     // Check to see if the user ate too much
     function checkIfTooMuch() {
         // Get the user innput
@@ -34,9 +37,20 @@ function LunchCheckController($scope){
         // Check for empyt string
 
         if (text == "") {
+            $scope.messageStyle = {
+                'color': 'red',
+                'border': '1px solid red',
+                'padding': '5px'
+            };
             $scope.message = "Please enter data first";
+            
             return false;
         } else {
+            $scope.messageStyle = {
+                'color': 'green',
+                'border': '1px solid green',
+                'padding': '5px'
+            };
             $scope.message = "";
             return true;
         }
