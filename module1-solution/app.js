cefaef4d0e27810ea65517e65b0ef0c43c77bda9
Troy Scott <consult@troyscott.ca>
2017-11-25 23:51:31 +0900

    added all 3 optional requirements

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,4 +1,10 @@
 // Lunch Check Application
+// Developed by: Troy Scott
+
+(function(){
+
+'strict';
+
 
 angular.module('LunchCheck',[ ])
 .controller('LunchCheckController', LunchCheckController);
@@ -11,7 +17,9 @@ function LunchCheckController($scope){
     $scope.message = "";
     $scope.lunchList = "";
     $scope.checkIfTooMuch = checkIfTooMuch;
-    // No style intially
+    // This variable is used for the optional styling requirements
+    // See index html.  This initializes/clears the style (ng-style)
+    // ng-style requires and object literal
     $scope.messageStyle = {};
     console.log($scope.messageStyle);
     
@@ -19,7 +27,6 @@ function LunchCheckController($scope){
     function checkIfTooMuch() {
         // Get the user innput
         var list = $scope.lunchList;
-        console.log(list);
         if (isValidEntry(list)) {
             if (itemCount(list) <= 3) {
                 $scope.message = "Enjoy!";
@@ -30,27 +37,50 @@ function LunchCheckController($scope){
     }
 
     function itemCount(text) {
-        return text.split(",").length;
+        
+        /* Optional Requirement
+
+            Do not count empty items:
+            item1, item2, , item3
+            Count should be 3 not 4 in this case.
+
+        */
+        var items = text.split(",");
+        console.log(items);
+        for (var i = 0;i < items.length; i++) {
+            console.log(items[i]);
+            if (items[i] == "") {
+                // remove empty items from the array
+                console.log('item is empty');
+                var index = items.indexOf(items[i]);
+                items.splice(index, 1);
+            }
+        }
+        console.log(items);
+        console.log(items.length);
+        return items.length;
     }
     // Validate user input
     function isValidEntry(text){
         // Check for empyt string
-
         if (text == "") {
+            // used by ng-style for optional requirements
             $scope.messageStyle = {
                 'color': 'red',
                 'border': '1px solid red',
                 'padding': '5px'
             };
+            // update message
             $scope.message = "Please enter data first";
-            
             return false;
         } else {
+            // used by ng-style for optional requirements
             $scope.messageStyle = {
                 'color': 'green',
                 'border': '1px solid green',
                 'padding': '5px'
             };
+            // upate message
             $scope.message = "";
             return true;
         }
@@ -59,4 +89,4 @@ function LunchCheckController($scope){
 } // Controller
 
 
-
+})(); // IFEE
